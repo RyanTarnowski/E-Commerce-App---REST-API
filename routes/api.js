6bcacb919a567a272cc39bb3da724f8f65ac0166
Ryan Tarnowski <ryan.tarnowski@gmail.com>
2025-09-14 08:47:32 -0600

    Added Swagger API documentation

--- routes/api.js
+++ routes/api.js
@@ -4,10 +4,14 @@ const userRouter = require('./users');
 const productRouter = require('./products');
 const cartRouter = require('./carts');
 const orderRouter = require('./orders');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../docs/E-Commerce API.json');
 
 apiRouter.use('/users', userRouter);
 apiRouter.use('/products', productRouter);
 apiRouter.use('/cart', cartRouter);
 apiRouter.use('/orders', orderRouter);
+apiRouter.use('/docs', swaggerUi.serve);
+apiRouter.get('/docs', swaggerUi.setup(swaggerDocument));
 
 module.exports = apiRouter;
